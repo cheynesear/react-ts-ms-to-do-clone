fd808368cf5db08e79f05a48fde8d5503a6412af
cheynesear <cheynesear@gmail.com>
2022-04-28 11:56:50 +0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+
+//Font Awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSun,
+  faCalendar,
+  faInfinity,
+  faFlag,
+  faHouse,
+  faList,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
@@ -8,29 +19,53 @@ function App() {
       {/* Universal elements */}
       <div className="side-bar">
         {/* container 1: header */}
-        <div className="container">
+        <div className="header">
           <div className="acc-circle">CS</div>
           <section>
             <p>Cheyne Sear</p>
             <p>cheynesear@gmail.com</p>
           </section>
         </div>
-        {/* container 2: search bar and tabs */}
+        {/* container 2: search bar */}
         <div className="container">
           <input type="text" />
+        </div>
+        {/* container 3: search bar */}
+        <div className="container">
+          {/* ACTION: get colour of icons working */}
           <ul>
-            <li><img src="" alt="icon"/>My Day</li>
-            <li><img src="" alt="icon"/>Planned</li>
-            <li><img src="" alt="icon"/>All</li>
-            <li><img src="" alt="icon"/>Flagged Email</li>
-            <li><img src="" alt="icon"/>Tasks</li>
+            <li>
+              <FontAwesomeIcon icon={faSun} /> My Day
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faCalendar} /> Planned
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faInfinity} /> All
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faFlag} /> Flagged Email
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faHouse} /> Tasks
+            </li>
+          </ul>
+        </div>
+        {/* container 4: horizontal line */}
+        <div className="container">
+          <hr />
+        </div>
+        {/* container 5: task lists */}
+        <div className="container">
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faList} /> Untiled List
+            </li>
           </ul>
         </div>
       </div>
-      <div className="add-task"></div>
+
       {/* My Day Page */}
-      
-      
     </div>
   );
 }
