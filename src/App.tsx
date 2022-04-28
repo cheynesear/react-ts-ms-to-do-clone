af08d984991a5bf7452030e26e7821e5748c9aab
cheynesear <cheynesear@gmail.com>
2022-04-28 10:21:00 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Universal elements */}
+      <div className="side-bar">
+        {/* container 1: header */}
+        <div className="container">
+          <div className="acc-circle">CS</div>
+          <section>
+            <p>Cheyne Sear</p>
+            <p>cheynesear@gmail.com</p>
+          </section>
+        </div>
+        {/* container 2: search bar and tabs */}
+        <div className="container">
+          <input type="text" />
+          <ul>
+            <li><img src="" alt="icon"/>My Day</li>
+            <li><img src="" alt="icon"/>Planned</li>
+            <li><img src="" alt="icon"/>All</li>
+            <li><img src="" alt="icon"/>Flagged Email</li>
+            <li><img src="" alt="icon"/>Tasks</li>
+          </ul>
+        </div>
+      </div>
+      <div className="add-task"></div>
+      {/* My Day Page */}
+      
+      
     </div>
   );
 }
