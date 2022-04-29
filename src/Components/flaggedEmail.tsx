8287f3daa1dba333d28b942d1102bd066eabae43
cheynesear <cheynesear@gmail.com>
2022-04-29 15:32:29 +0800

    commit

---
+++ src/Components/flaggedEmail.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FlaggedEmail() {
+    return (
+        null
+    )
+}
+
+export default FlaggedEmail;
---
+++ src/Components/myDay.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MyDay () {
+    return (
+    <div>
+        My Day
+    </div>
+    )
+}
+
+export default MyDay;
